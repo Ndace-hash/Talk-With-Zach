d3dd69edcead01ae790892b9ef39ca12c03b3222
Zaccheaus <zaccheausyisa00@gmail.com>
2023-04-26 18:51:38 +0100

    feat: Hero section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
-<script setup></script>
+<script setup>
+import AppHero from "./components/AppHero.vue";
+</script>
 
 <template>
-  <div>hello</div>
+  <div>
+    <AppHero />
+  </div>
 </template>
-
-<style scoped></style>
