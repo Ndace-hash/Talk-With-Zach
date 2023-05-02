885d84943e51faf9b8630772f6637db67bd520d0
Zaccheaus <zaccheausyisa00@gmail.com>
2023-05-02 19:56:23 +0100

    feat: added project section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import AppHero from "./components/AppHero.vue";
 import AppSkills from "./components/skills/AppSkills.vue";
+import AppProject from "./components/project/AppProject.vue";
 </script>
 
 <template>
-  <div>
+  <div class="mb-6">
     <AppHero />
     <AppSkills />
+    <AppProject />
   </div>
 </template>
